072e0a1231927864fc77821becfcf86f768daf43
Roberto Estrada <restrada@paridin.com>
2020-05-18 23:27:36 -0500

    added run lookup

---
+++ lib/mix/tasks/dino.ex
@@ -0,0 +1,28 @@
+defmodule Mix.Tasks.Dino do
+  use Mix.Task
+
+  @shortdoc "Prints Dino help information (Based on Phoenix Framework tasks, it was customized using the dino style)."
+
+  @moduledoc """
+  Prints Phoenix tasks and their information.
+      mix dino
+  """
+
+  @doc false
+  def run(args) do
+    case args do
+      [] -> general()
+      _ -> Mix.raise "Invalid arguments, expected: mix dino."
+    end
+  end
+
+  defp general() do
+    Application.ensure_all_started(:phoenix)
+    IO.inspect("I'm called")
+    Mix.shell().info "Phoenix v#{Application.spec(:phoenix, :vsn)}"
+    Mix.shell().info "Productive. Reliable. Fast."
+    Mix.shell().info "A productive web framework that does not compromise speed or maintainability."
+    Mix.shell().info "\nAvailable tasks:\n"
+    Mix.Tasks.Help.run(["--search", "dino."])
+  end
+end
