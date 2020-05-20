78278f056ea6aa8aab3ae695b3853d921a623304
Roberto Estrada <restrada@paridin.com>
2020-05-19 19:21:34 -0500

    don't start when is a dependency.

--- lib/application.ex
+++ lib/application.ex
@@ -6,7 +6,7 @@ defmodule DinoTasks.Application do
   use Application
 
   def start(_type, _args) do
-    children = [
+    _children = [
       # Start the Ecto repository
       DinoTasks.Repo,
       # Start the Telemetry supervisor
@@ -22,7 +22,8 @@ defmodule DinoTasks.Application do
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: DinoTasks.Supervisor]
-    Supervisor.start_link(children, opts)
+    # for testing I need to uncomment the _children and pass to start_link
+    Supervisor.start_link([], opts)
   end
 
   # Tell Phoenix to update the endpoint configuration
